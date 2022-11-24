b695fac1ba6e8ab46a1cfe3203114b261044db46
Sina Shadbad <shadbad@gmail.com>
2022-11-24 14:17:54 +0000

    update tests.

--- src/components/textbox/textbox.test.tsx
+++ src/components/textbox/textbox.test.tsx
@@ -4,14 +4,15 @@ import { Textbox } from './textbox';
 
 describe('<Textbox />', () => {
     it('renders correctly', () => {
-        render(<Textbox label="sample" />);
+        const onChange = jest.fn();
+        render(<Textbox label="sample" onChange={onChange} value="" />);
         expect(screen.getByPlaceholderText('sample')).toBeInTheDocument();
     });
 
     it('binds change event handler', async () => {
         user.setup();
         const onChange = jest.fn();
-        render(<Textbox label="sample" onChange={onChange} />);
+        render(<Textbox label="sample" onChange={onChange} value="" />);
         await user.type(screen.getByPlaceholderText('sample'), 'test');
         expect(onChange).toBeCalledTimes(4);
     });
